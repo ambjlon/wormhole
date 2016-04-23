c95c19e2ea1a49c6d4695dd32bef55b9990f845f
chenjianglong <chenjianglong@didichuxing.com>
2016-04-23 19:07:56 +0800

    add redis from company

---
+++ redis_6380/load.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+#
+# Simple Redis init.d script conceived to work on Linux systems
+# as it does use of the /proc filesystem.
+
+num=6380
+REDIS_HOME=/home/xiaoju/redis_6380/
+REDIS_PORT=$num
+EXEC=$REDIS_HOME/bin/redis-server
+CLIEXEC=$REDIS_HOME/bin/redis-cli
+
+PIDFILE=$REDIS_HOME/log/redis.pid
+CONF="$REDIS_HOME/conf/redis.conf"
+
+case "$1" in
+    start)
+        $EXEC $CONF
+        ;;
+    stop)
+        $CLIEXEC -p $num shutdown
+        ;;
+    *)
+        echo "Please use start or stop as first argument"
+        ;;
+esac
